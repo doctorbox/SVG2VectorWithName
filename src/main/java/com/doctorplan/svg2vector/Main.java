2a2636deabd8bd08007739b5afafbdeada9e36eb
ramt57 <ramt57@gmail.com>
2022-04-01 17:16:42 +0530

    changed main class

--- src/main/java/com/doctorplan/svg2vector/Main.java
+++ src/main/java/com/doctorplan/svg2vector/Main.java
@@ -7,7 +7,7 @@ import java.io.FileOutputStream;
 public class Main {
     public static void main(String[] args) {
         try {
-            String result = Svg2Vector.parseSvgToXml(new File("body_map_rash.svg"), new FileOutputStream("body_map_rash.xml"));
+            String result = Svg2Vector.parseSvgToXml(new File(args[0]), new FileOutputStream(args[1]));
             System.out.println(result);
         } catch ( Exception e) {
             e.printStackTrace();
